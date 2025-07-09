7cb05684e49dff84aee5b5700301bc5af002c80f
balong <balong85@gmail.com>
2025-07-09 10:21:44 -0400

    fix: darken main header text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <main className="h-screen flex flex-col">
       <header className="p-4 lg:p-6 w-full flex justify-center items-center bg-white shadow-md z-10 shrink-0 relative">
         <div className="text-center">
-            <h1 className="text-4xl font-bold font-serif">DCNYC</h1>
+            <h1 className="text-4xl font-bold font-serif text-gray-900">DCNYC</h1>
             <p className="text-sm text-gray-900 font-sans">New York Diet Coke Reviews</p>
         </div>
         <Link href="/about" className="absolute top-1/2 right-4 lg:right-6 -translate-y-1/2 text-sm text-gray-900 hover:text-black font-sans">
